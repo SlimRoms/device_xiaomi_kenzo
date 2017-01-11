8a7fcc7184f14f36e17cea0b2790bc61d49646fe
minz1 <minz1@github.com>
2017-01-10 23:10:58 -0500

    kenzo: add vulkan support
    Change-Id: I917db2146ec427807c3c43ede98901df67f3811d

--- device.mk
+++ device.mk
@@ -47,3 +47,9 @@ PRODUCT_COPY_FILES += \
 
 # Inherit from msm8956-common
 $(call inherit-product, device/xiaomi/msm8956-common/msm8956.mk)
+
+# Vulkan
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+
